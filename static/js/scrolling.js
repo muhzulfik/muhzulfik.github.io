bd59722d2e0321259c80058bd57d1dd12cf2429e
muhzulfik <72639811+muhzulfik@users.noreply.github.com>
2020-10-13 11:13:04 +0700

    initial commit

---
+++ static/js/scrolling.js
@@ -0,0 +1,10 @@
+$(window).scroll(function(){
+    var scroll = $(window).scrollTop();
+    console.log(scroll)
+
+    if(scroll >= 250){
+        $("#myNav").addClass("bg-dark");
+    } else {
+        $("#myNav").removeClass("bg-dark");
+    }
+});
